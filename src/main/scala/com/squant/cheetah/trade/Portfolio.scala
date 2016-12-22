06963f4bb514789f9aa4e17b06f1f713648808f8
牧仁 <xuqi.xq@alibaba-inc.com>
2016-12-22 14:37:48 +0800

    完善个人账户信息

--- src/main/scala/com/squant/cheetah/trade/Portfolio.scala
+++ src/main/scala/com/squant/cheetah/trade/Portfolio.scala
@@ -2,16 +2,17 @@ package com.squant.cheetah.trade
 
 import java.time.LocalDateTime
 
+import scala.collection._
+
 //账户当前的资金，标的信息，即所有标的操作仓位的信息汇总
 class Portfolio {
-  private var status: Record = null
-  //需要在初始化时赋值
-  //  private val riskAnalysis: RiskAnalysis = new RiskAnalysis
-  //记录各个时间点账户状态
-  private val records = Map[LocalDateTime, Record]()
-  //最后更新record的时间点
-  private var ts: LocalDateTime = null
-  //记录账户当前持仓情况
-  private var positions: Map[String, Position] = Map[String, Position]()
+
+  val porfolioMetric:PortfolioMetric = new PortfolioMetric
+
+  val records = mutable.Map[LocalDateTime, Record]() //记录各个时间点账户状态
+
+  var ts: LocalDateTime = null //最后更新record的时间点
+
+  var positions: Map[String, Position] = mutable.Map[String, Position]() //记录账户当前持仓情况
 
 }
