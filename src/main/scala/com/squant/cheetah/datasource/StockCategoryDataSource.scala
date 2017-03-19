1cc8e710fffd7ef9ec980854f629cbbda96e9714
eryk <xuqi86@gmail.com>
2017-03-20 00:06:28 +0800

    +datasource&database #15 add toDB method to save stock category datasource

--- src/main/scala/com/squant/cheetah/datasource/StockCategoryDataSource.scala
+++ src/main/scala/com/squant/cheetah/datasource/StockCategoryDataSource.scala
@@ -4,6 +4,7 @@ import java.io.{File, FileNotFoundException, PrintWriter}
 import java.time.LocalDateTime
 
 import com.squant.cheetah.domain.Category
+import com.squant.cheetah.engine.{DataBase, Row}
 import com.squant.cheetah.utils.Constants._
 import com.squant.cheetah.utils._
 import com.typesafe.scalalogging.LazyLogging
@@ -15,31 +16,28 @@ import scala.io.Source
 
 object StockCategoryDataSource extends DataSource with LazyLogging {
 
+  private val baseDir = config.getString(CONFIG_PATH_DB_BASE)
+  private val fileName = config.getString(CONFIG_PATH_CATEGORY)
+  private val tableName = "stock_category"
+
   //初始化数据源
   override def init(): Unit = {
     clear()
     update(LocalDateTime.now, LocalDateTime.now)
   }
 
-  def update(start: LocalDateTime, stop: LocalDateTime) = {
+  def update(start: LocalDateTime = LocalDateTime.now(), stop: LocalDateTime = LocalDateTime.now()) = {
     //下载股票分类数据
-    saveCategory(config.getString(CONFIG_PATH_DB_BASE) + config.getString(CONFIG_PATH_CATEGORY))
+    toCSV(baseDir + fileName)
+
+    toDB(tableName, DataBase.getEngine)
   }
 
   override def clear(): Unit = {
-    rm(config.getString(CONFIG_PATH_DB_BASE) + config.getString(CONFIG_PATH_CATEGORY))
+    rm(baseDir + fileName)
       .foreach(r => logger.info(s"delete ${r._1} ${r._2}"))
-  }
 
-  private def getSource(url: String, encode: String): Option[String] = {
-    for (i <- 1 to 3)
-      try {
-        return Some(Source.fromURL(url, encode).mkString)
-      } catch {
-        case ex: FileNotFoundException => if (i == 3) logger.error("page not found:" + url)
-        case ex: Exception => if (i == 3) logger.error("fail to get source:" + url, ex.getMessage)
-      }
-    None
+    DataBase.getEngine.deleteTable(tableName)
   }
 
   def hy(): Map[String, Category] = {
@@ -60,7 +58,56 @@ object StockCategoryDataSource extends DataSource with LazyLogging {
     map.toMap[String, Category]
   }
 
-  def urlToCategoryMap(map: mutable.HashMap[String, Category], url: String, categoryType: String): Unit = {
+
+  def toCSV(file: String): Unit = {
+    val writer = new PrintWriter(new File(file))
+    val map: Map[String, Category] = hy() ++ gn()
+
+    map.foreach(item => {
+      val stocks = item._2.stocks.foldLeft("") { (a, b) =>
+        a + "_" + b
+      }
+      writer.write(s"${new String(item._1.getBytes("utf8"))},${item._2.code},${item._2.categoryType},${item._2.pinyin},${item._2.url},$stocks\n")
+    })
+    writer.close()
+  }
+
+  def toDB(tableName: String, engine: DataBase): Unit = {
+    val categories: Map[String, Category] = readCategory(baseDir + fileName)
+
+    val rows: Map[String, Row] = categories.map(item => (item._1, Category.categoryToRow(item._2)))
+
+    rows.foreach(item => {
+      println(s"${item._1}")
+      println(s"${item._2}")
+    }
+    )
+
+    engine.toDB(tableName, rows.values.toList)
+  }
+
+  def readCategory(file: String): Map[String, Category] = {
+    val lines = Source.fromFile(file).getLines()
+    val map = mutable.Map[String, Category]()
+    for (line <- lines) {
+      val fields = line.split(",", 6)
+      map.put(fields(0), Category(fields(0), fields(1), fields(2), fields(3), fields(4), fields(5).split("_").filter(_ != "").toList))
+    }
+    map.toMap
+  }
+
+  private def getSource(url: String, encode: String): Option[String] = {
+    for (i <- 1 to 3)
+      try {
+        return Some(Source.fromURL(url, encode).mkString)
+      } catch {
+        case ex: FileNotFoundException => if (i == 3) logger.error("page not found:" + url)
+        case ex: Exception => if (i == 3) logger.error("fail to get source:" + url, ex.getMessage)
+      }
+    None
+  }
+
+  private def urlToCategoryMap(map: mutable.HashMap[String, Category], url: String, categoryType: String): Unit = {
     getSource(url, "gbk") match {
       case source: Some[String] => {
         val elements = Jsoup.parse(source.get).select("table tbody").get(0).select("tr").iterator()
@@ -100,7 +147,7 @@ object StockCategoryDataSource extends DataSource with LazyLogging {
     }
   }
 
-  def parseToCategory(name: String, url: String, categoryType: String): Category = {
+  private def parseToCategory(name: String, url: String, categoryType: String): Category = {
     val pinyin = categoryType match {
       case "gn" => url.replace("http://q.10jqka.com.cn/stock/gn/", "").replace("/", "")
       case "hy" => url.replace("http://q.10jqka.com.cn/stock/thshy/", "").replace("/", "")
@@ -119,27 +166,5 @@ object StockCategoryDataSource extends DataSource with LazyLogging {
     }
   }
 
-  def saveCategory(file: String): Unit = {
-    val writer = new PrintWriter(new File(file))
-    val map: Map[String, Category] = hy() ++ gn()
-
-    map.foreach(item => {
-      val stocks = item._2.stocks.foldLeft("") { (a, b) =>
-        a + "_" + b
-      }
-      writer.write(s"${item._1},${item._2.code},${item._2.categoryType},${item._2.pinyin},${item._2.url},$stocks\n")
-    })
-    writer.close()
-  }
-
-  def readCategory(file: String): Map[String, Category] = {
-    val lines = Source.fromFile(file).getLines()
-    val map = mutable.Map[String, Category]()
-    for (line <- lines) {
-      val fields = line.split(",", 6)
-      map.put(fields(0), Category(fields(0), fields(1), fields(2), fields(3), fields(4), fields(5).split("_").filter(_ != "").toList))
-    }
-    map.toMap
-  }
 }
 
--- src/main/scala/com/squant/cheetah/domain/Category.scala
+++ src/main/scala/com/squant/cheetah/domain/Category.scala
@@ -1,3 +1,29 @@
 package com.squant.cheetah.domain
 
+import java.time.LocalDateTime
+
+import com.squant.cheetah.engine.Row
+import com.squant.cheetah.utils._
+
 case class Category(name: String, code: String, categoryType: String, pinyin: String, url: String, stocks: List[String])
+
+object Category {
+  def categoryToRow(category: Category): Row = {
+    val map = scala.collection.mutable.HashMap[String, String]()
+    map.put("name", category.name)
+    map.put("code", category.code)
+    map.put("categoryType", category.categoryType)
+    map.put("pinyin", category.pinyin)
+    map.put("url", category.url)
+
+    val stocks = category.stocks.foldLeft("") { (a, b) =>
+      a + "_" + b
+    }
+
+    map.put("stocks", stocks)
+
+    Row(category.name, localDateTimeToLong(LocalDateTime.now()), map.toMap)
+  }
+
+  def rowToCategory(row:Row):Category = ???
+}
