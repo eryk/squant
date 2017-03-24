7f196141ad0a307d6acafdda73ea2135c4c34b94
eryk <xuqi86@gmail.com>
2017-03-24 21:27:42 +0800

    +docker #14 update Dockerfile

---
+++ docker/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/local/hbase-1.3.0/bin/start-hbase.sh
+/usr/local/jdk1.8.0_121/bin/java -cp /home/squant/conf:/home/squant/squant-assembly-1.0.jar com.squant.cheetah.datasource.Updater
