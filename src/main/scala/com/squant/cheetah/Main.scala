01cc6276643fc5116405e83559b85a9286acefba
牧仁 <xuqi.xq@alibaba-inc.com>
2017-04-01 10:14:21 +0800

    +datasource #  add command line parser

--- src/main/scala/com/squant/cheetah/Main.scala
+++ src/main/scala/com/squant/cheetah/Main.scala
@@ -5,8 +5,12 @@ import com.squant.cheetah.utils._
 
 object Main extends App {
 
-  trait Command {
+  sealed trait Command {
     def run()
+  }
+
+  case object DummyCommand extends Command {
+    override def run(): Unit = ???
   }
 
   case class Source(sourceType: String = "", config: TaskConfig = TaskConfig(), isInit: Boolean = false) extends Command {
@@ -36,46 +40,80 @@ object Main extends App {
           StockCategoryDataSource.update(config)
           TickDataSource.update(config)
         }
-//        case _ => throw new NoSuchMethodError()
+        case _ => throw new NoSuchMethodError()
       }
     }
   }
 
-  var config = TaskConfig(toCSV = true)
-  var source = Source()
+  var config = TaskConfig(clear = true, toCSV = true, toDB = false)
+  var source = Source(isInit = false)
 
-  val parser = new scopt.OptionParser[Unit]("squant") {
+  val sep = "\n\t\t\t\t\t\t\t"
+
+  val parser = new scopt.OptionParser[Command]("squant") {
     head("squant", "1.x")
+    help("help").abbr("h").text("display this help and exit")
 
-    cmd("source").foreach(_ => Source()).
+    cmd("source").action((_, c) => Source()).
       text("init or download data source").
       children(
-        opt[String]("type").abbr("t").text("set datasource to be downloaded")
-            .minOccurs(1).maxOccurs(1)
-          .foreach((sType: String) => {
-            source = source.copy(sourceType = sType)
+        opt[String]("type").abbr("t").text(
+          s"set datasource to be downloaded,${sep}support: " +
+            s"${sep}\tall: all kind of datasource," +
+            s"${sep}\tbasic: stock list with basic info," +
+            s"${sep}\tcategory: stock category data," +
+            s"${sep}\tdaily: stock day bar," +
+            s"${sep}\tfinance: Financial Statements," +
+            s"${sep}\tminute: stock minute bar,include 5/15/30/60 minutes," +
+            s"${sep}\tmoneyflow: stock money flow data," +
+            s"${sep}\ttick: stock tick data")
+          .minOccurs(1).maxOccurs(1)
+          .action({
+            case (s: String, c: Source) => c.copy(sourceType = s)
+            case _ => throw new IllegalStateException
           }),
-        opt[Boolean]("init").text("init data from source").foreach((value: Boolean) => {
-          source = source.copy(isInit = value)
+        opt[Boolean]("init").text("if true,init data from source, default:false").action({
+          case (value: Boolean, c: Source) => {
+            c.copy(isInit = value)
+          }
+          case _ => throw new IllegalStateException
         }),
-        opt[String]("start").text("set download start time").foreach((value) =>
-          config = config.copy(start = stringToLocalDateTime(value, "yyyyMMdd"))),
-        opt[String]("stop").text("set download stop time").foreach((value) =>
-          config = config.copy(stop = stringToLocalDateTime(value, "yyyyMMdd"))),
-        opt[Boolean]("clear").text("if true,clean data dir before download").foreach((value: Boolean) => {
-          config = config.copy(clear = value)
+        opt[String]("start").text("format:yyyyMMdd, set download start time").action({
+          case (value: String, c: Source) => {
+            config = config.copy(start = stringToLocalDateTime(value, "yyyyMMdd"))
+            c.copy(config = config)
+          }
+          case _ => throw new IllegalStateException
         }),
-        opt[Boolean]("toDB").text("if true,save downloaded data to hbase").foreach((value: Boolean) => {
-          config = config.copy(toDB = value)
+        opt[String]("stop").text("format:yyyyMMdd, set download stop time").action({
+          case (value: String, c: Source) => {
+            config = config.copy(stop = stringToLocalDateTime(value, "yyyyMMdd"))
+            c.copy(config = config)
+          }
+          case _ => throw new IllegalStateException
+        }),
+        opt[Boolean]("clear").text("if true,clean data dir before download, default:true").action({
+          case (value: Boolean, c: Source) => {
+            config = config.copy(clear = value)
+            c.copy(config = config)
+          }
+        }),
+        opt[Boolean]("toDB").hidden().text("if true,save downloaded data to hbase, default:false").action({
+          case (value: Boolean, c: Source) => {
+            config = config.copy(toDB = value)
+            c.copy(config = config)
+          }
         })
       )
+    cmd("test")
+    cmd("run")
   }
 
-  if (parser.parse(args)) {
-    source = source.copy(config = config)
-    source.run()
-  }
-  else {
-//    parser.showUsage()
+  parser.parse(args, DummyCommand) match {
+    case Some(DummyCommand) | None => {
+      parser.showUsage()
+      System.exit(-1)
+    }
+    case Some(c) => c.run()
   }
 }
