855928ac2deaa9d48dd77e45417e28672ab9a426
eryk <xuqi86@gmail.com>
2017-04-14 08:19:20 +0800

    +backtest # add test

---
+++ src/test/scala/StockSelector.scala
@@ -0,0 +1,41 @@
+import java.time.LocalDateTime
+
+import com.squant.cheetah.DataEngine
+import com.squant.cheetah.domain.DAY
+
+/**
+  * Created by eryk on 17-4-13.
+  */
+object StockSelector extends App {
+
+  var symbols = DataEngine.symbols()
+
+  symbols = symbols.filter(symbol => !symbol.name.contains("ST") && symbol.pe > 0 && symbol.pe < 200 && DataEngine.realtime(symbol.code).close < 30)
+  symbols.par.foreach(symbol => find(symbol.code))
+
+  def find(symbol: String) = {
+    try {
+      val bars = DataEngine.ktype(symbol, DAY, index = false).filter(bar => bar.close > 0)
+      if (bars.length > 13) {
+        for (i <- 13 until bars.size) {
+
+          var loop = i
+          var signal = true
+          while (loop > 0 && signal) {
+            if (bars(loop).close > bars(loop - 4).close)
+              signal = false
+            loop -= 1
+            if (loop == i - 9 && signal && bars(i).date.toLocalDate.isEqual(LocalDateTime.now().plusDays(-2).toLocalDate)) {
+              println(bars(i).code + "\t" + bars(i).date)
+            }
+          }
+        }
+      }
+    } catch {
+      case ex: Exception =>
+    }
+
+
+  }
+
+}
